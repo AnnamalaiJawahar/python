b67d138819cf4ab909f6b72bf566e1ed624f1fb7
Annamalai Jawahar <annamalaijawahar@outlook.com>
2022-05-26 13:29:21 -0500

    Git tracking print

---
+++ Practicecopy.py
@@ -0,0 +1,11 @@
+#print("Hello World")
+#print("I am Iron man")
+#print("I am Iron man")
+#print("Hello World,\nI am Iron man\nI am Tony Stark \n")
+#print("Hello\n"*100)
+
+print("Hello, Welcome to Coffee shop!!!\n")
+
+a = input("What is your name? ")
+print("Hello", a, "Thank you for coming today")
+#print("Hello " + a + " Thank you for coming today")
